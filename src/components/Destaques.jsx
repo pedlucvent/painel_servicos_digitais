5e2a8fdee0e90a1fba0b64de2ec0cd4b7773c72b
pedlucvent <pedrolucass602venturameyer@gmail.com>
2026-05-18 23:26:43 -0300

    Finalização do painel de serviços digitais

---
+++ src/components/Destaques.jsx
@@ -0,0 +1,34 @@
+function Destaques(){
+
+return(
+
+<section className="destaques">
+
+<div>
+
+<h2>+15 mil</h2>
+<p>Usuários ativos</p>
+
+</div>
+
+<div>
+
+<h2>98%</h2>
+<p>Satisfação</p>
+
+</div>
+
+<div>
+
+<h2>24/7</h2>
+<p>Suporte disponível</p>
+
+</div>
+
+</section>
+
+)
+
+}
+
+export default Destaques
